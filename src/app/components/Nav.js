4c11af7db9f8e6d9b4a1ad4839b7b303a53cdf18
Jaegar Saunders <jaegarsaunders@192.168.219.9>
2024-01-26 12:27:12 -0330

    Responsive navbar complete

--- src/app/components/Nav.js
+++ src/app/components/Nav.js
@@ -1,36 +1,58 @@
-"use client"
-import MenuIcon from "./MenuIcon"
-import {useState} from "react"
+"use client";
+import { RiMenu5Line } from "react-icons/ri";
+import { useState } from "react";
 
-const Menu = () => {
-    return (
-        <div></div>
-    )
-}
+
+
+const Menu = ({menuClass}) => {
+  return (
+    <div className={`menu flex justify-end items-end text-end md:hidden absolute right-0 top-[-20px] w-[125px] p-[2rem] bg-primary z-40 transition-all ${menuClass}`}>
+      <ul className="block my-[10vh]">
+        <li className="hover:underline hover:cursor-pointer">
+          <a href="#">home</a>
+        </li>
+        <li className="hover:underline hover:cursor-pointer">
+          <a href="#">projects</a>
+        </li>
+        <li className="hover:underline hover:cursor-pointer">
+          <a href="#">contact</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default function Nav() {
-    const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toggleNav = () => {
-        setIsOpen(!isOpen)
-    }
+  const toggleNav = () => {
+    setIsOpen(!isOpen);
+  };
 
-    return (
-        <header className="sticky top-0 z-[20] mx-auto flex w-full items-center justify-between">
-            <div></div>
-            <nav className="w-1/3">
-                <ul className="hidden justify-between md:flex">
-                    <li className="">home</li>
-                    <li className="">projects</li>
-                    <li className="">contact</li>
-                </ul>
-                <div className="mobile md:hidden">
-                    <button onClick={toggleNav} className={`${isOpen ? "block" : <Menu/>}`}>
-                        <MenuIcon/>
-                    </button>
-                </div>
-            </nav>
-        </header>
-    )
+  let menuClass = isOpen ? "translate-x-0" : "translate-x-full";
+  let buttonClass = isOpen ? "rotate-90" : "rotate-0";
+
+  return (
+    <nav className="p-[2rem] fixed top-0 left-0 flex w-full z-50">
+      <div className="logo flex grow justifty-start items-center"></div>
+      <div className="links flex max-w-1/2 md:w-1/3 lg:w-1/4">
+        <ul className="hidden items-center justify-evenly md:flex w-full">
+          <li className="hover:underline hover:cursor-pointer">
+            <a href="#">home</a>
+          </li>
+          <li className="hover:underline hover:cursor-pointer">
+            <a href="#">projects</a>
+          </li>
+          <li className="hover:underline hover:cursor-pointer">
+            <a href="#">contact</a>
+          </li>
+        </ul>
+        <button className={`z-50`} onClick={toggleNav}>
+          <RiMenu5Line className={`block md:hidden z-50 transition-all ${buttonClass}`} size={25} />
+        </button>
+      </div>
+
+      <Menu menuClass={menuClass}/>
+    </nav>
+  );
 }
-
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,12 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    colors: {
+      primary: '#3b28cc',
+      secondary: '#adb3cb',
+      white: '#fff',
+      dark: '#0B091A'
+    }
   },
   plugins: [],
 };
